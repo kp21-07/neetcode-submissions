51864470443c723dc67b956c6c7515f354bcd454
Kalakuntla Parjanya <kparjanya21@gmail.com>
2026-07-04 17:13:08 +0530

    Update: spiral-matrix - submission-0

--- Data Structures & Algorithms/spiral-matrix/submission-0.cpp
+++ Data Structures & Algorithms/spiral-matrix/submission-0.cpp
@@ -1,16 +1,63 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
+        vector<int> ans(m*n, 0);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < m; j++) {
-                swap(matrix[i][j], matrix[j][i]);
+        int i = 0, j = 0;
+        int UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
+        int direction = RIGHT;
+
+        int UP_WALL = 0;
+        int RIGHT_WALL = n;
+        int DOWN_WALL = m;
+        int LEFT_WALL = -1;
+        int ct = 0;
+
+        while (ct != m*n){
+            if (direction == RIGHT){
+                while (j < RIGHT_WALL){
+                    ans[ct] = matrix[i][j];
+                    j++;
+                    ct++;
+                }
+                i++;
+                j--;
+                RIGHT_WALL--;
+                direction = DOWN;
+            } else if (direction == DOWN){
+                while (i < DOWN_WALL){
+                    ans[ct] = matrix[i][j];
+                    i++;
+                    ct++;
+                }
+                i--;
+                j--;
+                DOWN_WALL--;
+                direction = LEFT;
+            } else if (direction == LEFT){
+                while (j > LEFT_WALL){
+                    ans[ct] = matrix[i][j];
+                    j--;
+                    ct++;
+                }
+                i--;
+                j++;
+                LEFT_WALL++;
+                direction = UP;
+            } else {
+                while (i > UP_WALL){
+                    ans[ct] = matrix[i][j];
+                    i--;
+                    ct++;
+                }
+                i++;
+                j++;
+                UP_WALL++;
+                direction = RIGHT;
             }
         }
-
-        for (auto& row : matrix) {
-            reverse(row.begin(), row.end());
-        }
+        
+        return ans; 
     }
 };
