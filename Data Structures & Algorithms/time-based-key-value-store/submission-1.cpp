226832aa6d0c06410163af6de474c44ee98ccc7f
Kalakuntla Parjanya <kparjanya21@gmail.com>
2026-07-05 16:51:19 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,20 @@
+class TimeMap {
+    unordered_map<string, unordered_map<int, vector<string>>> mp;
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        mp[key][timestamp].push_back(value);
+    }
+    
+    string get(string key, int timestamp) {
+        if (mp.find(key) == mp.end()) return "";
+
+        int seen = 0;
+        for (const auto& [time, _] : mp[key]) {
+            if (time <= timestamp) seen = max(seen, time);
+        }
+
+        return seen == 0 ? "" : mp[key][seen].back();
+    }
+};
